978258a19b55321488f5ffe88d8538b397874b4d
pcriadoperez <pablo@criadoperez.com>
2016-12-05 17:47:51 -0500

    Fixed Build Bug

--- SafeScan/Assets/FolderTreeItem.cs
+++ SafeScan/Assets/FolderTreeItem.cs
@@ -15,9 +15,12 @@ public class FolderTreeItem : MonoBehaviour {
 	}
     public void OnInputChange(string value)
     {
-        ItemContainer itemContainer = this.GetComponent<ItemContainer>();
-        GameObject item = (GameObject) itemContainer.Item;
-        item.name = value;
-
+        if(this.GetComponent<ItemContainer>()!= null)
+        {
+            ItemContainer itemContainer = this.GetComponent<ItemContainer>();
+            GameObject item = (GameObject)itemContainer.Item;
+            item.name = value;
+        }
+            
     }
 }
