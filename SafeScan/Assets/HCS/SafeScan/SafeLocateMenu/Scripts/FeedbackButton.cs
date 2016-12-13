1408da83a639c1920c18d7b8b7bca7e14c3c7e2a
pcriadoperez <pablo@criadoperez.com>
2016-12-13 10:31:08 -0500

    Added help button with link to hcs support website, and added close button from alert panel

---
+++ SafeScan/Assets/HCS/SafeScan/SafeLocateMenu/Scripts/FeedbackButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeedbackButton : MonoBehaviour {
+
+    public void OnClick_Handler()
+    {
+        Application.OpenURL("http://www.hcsafety.com/support/");
+    }
+}
